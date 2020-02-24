7c97bc44a33fef1f8eab579c52040d3684f5b4fc
saleem <saleemlbk666@gmail.com>
2020-02-24 11:48:37 +0530

    My First Commit

---
+++ src/test/java/io/javabrains/springbootquickstart/springapi/SpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springbootquickstart.springapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
